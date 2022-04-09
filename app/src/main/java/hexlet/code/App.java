df8d4eb4168cea3a97dbbfc7ded5276ecdb667d6
Danil Vinogradov <vinogradovspbu@gmail.com>
2022-04-10 00:07:57 +0300

    feat: поменять старый хеллоуворлд на новый

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -4,11 +4,9 @@
 package hexlet.code;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        System.out.println("Hello, World!");
     }
+
 }
