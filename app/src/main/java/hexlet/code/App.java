c3a16f18f1e6a54a490fdec7dfb6427b762fa9ca
Danil Vinogradov <vinogradovspbu@gmail.com>
2022-04-10 00:10:33 +0300

    feat: изменяет консольное сообщение

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -6,7 +6,7 @@ package hexlet.code;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Welcome to the Brain Games!");
     }
 
 }
